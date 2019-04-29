6bfc78a47f66f73198267ae67e4434b59d0d17cd
Rafae Ashraf <rafae.pooba@gmail.com>
2019-04-29 13:55:20 -0400

    Updates

---
+++ precache-manifest.f95078d070e413e2631eb80f25f13436.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89094844f7542524fee09c607642949",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "866e1fc503e0937e3d12",
+    "url": "/react-tutorial/static/css/main.3c942847.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "866e1fc503e0937e3d12",
+    "url": "/react-tutorial/static/js/main.b99b1fb6.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
