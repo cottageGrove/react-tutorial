ab153e5639ba64742a7ea258175c9a9a0d6bd392
Rafae Ashraf <rafae.pooba@gmail.com>
2019-04-29 14:02:16 -0400

    adding all react project files to repo

---
+++ src/ClassComponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from React
+
+const SimpleComponent = () => {
+    return <div>Example</div>
+}
+
+class ClassComponent extends Component {
+    render() {
+        <div>Example</div>
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+// import App from './App'
+import './index.css'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const heading = <div className="App">
+                    <h1>Hello I'm a bum</h1>
+                </div>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// class App extends Component {
+//     render() {
+//         return (
+//             heading
+//             // <div className="App">
+//             //     <h1>Hello I'm a bum! </h1>
+//             // </div>
+//         )
+//     }
+// }
+
+//Using JSX
+
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+ReactDOM.render(<App />, document.getElementById('root'))
